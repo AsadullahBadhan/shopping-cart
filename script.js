766fbb85f597dd48c5455afaee8c948f01c07a8f
AsadullahBadhan <asadullah.badhan@gmail.com>
2021-09-04 04:01:31 +0600

    case item btn handler fixed

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const phonePlusBtn = document.getElementById('phone-plus-btn');
 phonePlusBtn.addEventListener('click', () => plusBtnHandler('phone-count', 'phone-price'));
 phoneMinusBtn.addEventListener('click', () => minusBtnHandler('phone-count', 'phone-price'));
 
+
+//case button handler
+const caseMinusBtn = document.getElementById('case-minus-btn');
+const casePlusBtn = document.getElementById('case-plus-btn');
+casePlusBtn.addEventListener('click', () => plusBtnHandler('case-count', 'case-price'));
+caseMinusBtn.addEventListener('click', () => minusBtnHandler('case-count', 'case-price'));
+
 function plusBtnHandler(countId, priceId) {
     const count = document.getElementById(countId);
     let countNum = parseInt(count.value);
@@ -27,9 +34,11 @@ function minusBtnHandler(countId, priceId) {
 
 function priceCalculator(priceId, count) {
     let unitPrice;
-    if (priceId = 'phone-price') {
+    console.log(priceId);
+    if (priceId === 'phone-price') {
         unitPrice = 1219;
-    } else {
+    }
+    else if (priceId === 'case-price') {
         unitPrice = 59;
     }
     price = count * unitPrice;
